6a21249997eec9ab1f3f3e27c6d8f685a152bb07
Eddie Yao <adpoyao@gmail.com>
2017-09-28 09:17:18 -0700

    first attempt at Image Carousel

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here.
+// when a user selects one of the thumbnail images using the mouse or keyboard, that image should be displayed in the full size image container at the top. 
+// Updating the image should also update its alt attribute. 
+// To test keyboard interactivity, you can Tab to one of the thumbnails and press the Enter key.
+
+// listen to event that 'clicks' on thumbnails
+// .hero > change img src = thumbnail src
+// also replace alt
+'use strict';
+
+$(function(){
+  $('.thumbnails').on('click','.thumbnail', function(event){
+    let clickedImgSrc = $(event.currentTarget).find($('img').attr('src'));
+    let clickedImgAlt = $(event.currentTarget).find($('img').attr('alt'));
+    $('.hero').html(`<img src=${clickedImgSrc} alt=${clickedImgAlt}`);
+  });
+}
+);
